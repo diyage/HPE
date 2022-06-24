f1a0b5548a41823d75ec658e997f45f989979709
diyage <diyage0418@163.com>
2022-06-24 18:54:52 +0800

    2022-06-24-18:54

--- CPM/temp2.py
+++ CPM/temp2.py
@@ -4,32 +4,15 @@ import torch
 import torch.nn as nn
 from CPM.tools.dataset_define import LspDataSet
 
-# data_set_config = DataSetConfig()
-#
-# m = CPMNet(data_set_config)
-#
-# x = torch.rand(size=(2, 3, 368, 368))
-# center_map = torch.rand(size=(2, 368, 368))
-# out = m(x, center_map)
-# print(out.shape)
-
-from torch.utils.data import DataLoader
-d = LspDataSet(root='E:\PyCharm\DataSet\lsp')
-dl = DataLoader(d, batch_size=2, shuffle=False)
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
+data_set_config = DataSetConfig()
+data_set_config.sigma = 0.01
 
-def get_matrix_max_pos(matrix: torch.Tensor)->tuple:
-    max_index = matrix.argmax()
-    tmp = torch.zeros(*matrix.shape).view(-1)
-    tmp[max_index] = 1.0
-    tmp = tmp.view(*matrix.shape)
-    # get r/c
-    index_r = tmp.sum(dim=1).argmax()  # r
-    index_c = tmp.sum(dim=0).argmax()  # c
-
-    return int(index_c.item()), int(index_r.item())
+d = LspDataSet(root='E:\PyCharm\DataSet\lsp', data_set_opt=data_set_config)
+dl = DataLoader(d, batch_size=2, shuffle=False)
 
 
 for _, info in enumerate(dl):
