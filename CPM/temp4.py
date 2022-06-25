9501aef126614181a0d91478ba3db4fe2477c950
diyage <diyage0418@163.com>
2022-06-25 18:42:06 +0800

    2022-06-25-18:42

--- CPM/temp4.py
+++ CPM/temp4.py
@@ -13,12 +13,12 @@ from CPM.tools.visualize import Vis
 from DeepPose.tools.cv2_ import CV2
 from CPM.tools.dataset_define import guassian_kernel
 
-m = CPMNet(data_set_config=DataSetConfig())
-d = LspDataSet(root='E:\PyCharm\DataSet\lsp', use_visibility=True, train=True)
-dl = DataLoader(d, batch_size=2, shuffle=False)
+m = CPMNet(data_set_config=DataSetConfig()).to('cuda:1')
+d = LspDataSet(root=DataSetConfig.root_path, use_visibility=True, train=True)
+dl = DataLoader(d, batch_size=DataSetConfig.BATCH_SIZE, shuffle=False)
 
-d_2 = LspDataSet(root='E:\PyCharm\DataSet\lsp', use_visibility=True, train=False)
-dl_2 = DataLoader(d_2, batch_size=2, shuffle=False)
+d_2 = LspDataSet(root=DataSetConfig.root_path, use_visibility=True, train=False)
+dl_2 = DataLoader(d_2, batch_size=DataSetConfig.BATCH_SIZE, shuffle=False)
 
 from CPM.tools.evaluation import OKS
 
